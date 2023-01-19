b34d90debc0c5223b6d64a7328412d47a0ea8684
AlisonForsberg <aforsberg95@gmail.com>
2023-01-19 10:32:56 -0700

    comment

---
+++ tests/LoginApiBad.test.js
@@ -0,0 +1,17 @@
+import fetch from 'node-fetch';
+
+it ("Should get a login token", async () => { //async allows us to read from the database by moving on and coming back
+    const loginResponse = await fetch('https://dev.stedi.me/login', { //await fetch allows it to move on while getting the date
+    
+        method: 'POST', //method : POST says to give information to the server
+        headers:{
+            'Content-Type': 'application/text'
+        },
+
+        body:JSON.stringify({
+            "userName": "hotmail@gmail.com",
+            "password": "hjkl"
+        })
+    })
+    expect(loginResponse.status).toBe(401);  //200 means authorized
+})
